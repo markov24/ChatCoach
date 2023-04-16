185d0e0c46397917f4786a32ee9c533ff43d01eb
cakedefender <millerallanmichael2020@gmail.com>
2023-04-15 19:05:11 -0500

    lwjlkeq

--- gpt-test.py
+++ gpt-test.py
@@ -1,15 +1,17 @@
 import openai
 
 
-person = "Snoop Dog"
+person = "George Washington"
 
 openai.api_key = "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"
+audio_file = open("gettysburg_address.mp3", "rb")
+transcript = openai.Audio.transcribe(file=audio_file, model="whisper-1", response_format="text")
+
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": f"You are {person}"},
-        {"role": "user", "content": f"Write to me as {person}, using his style of grammar/tone. What's good snoop"},
-        {"role": "assistant", "content": "Yo yo yo, what's crackin' my homie? It's Snoop D-O-double-Gizzle up in this piece, ready to drop some knowledge on ya. What's good with you, my dude?"}
+        {"role": "user", "content": transcript}
   ]
 )
 
